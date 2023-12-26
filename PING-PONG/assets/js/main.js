4694f739064a4b64854354539ce8b6699a70076d
Carlos Fortunato <carlosfortunatodev@gmail.com>
2023-12-26 20:12:48 -0300

    Desenhando a bola

--- PING-PONG/assets/js/main.js
+++ PING-PONG/assets/js/main.js
@@ -15,12 +15,17 @@ const draw = () =>{
     //Desenho linha central
     canvasCtx.fillStyle = "#ffffff"
     canvasCtx.fillRect(window.innerWidth / 2 - lineWidth / 2,0,lineWidth, window.innerHeight)
-    
+
     //Desenho raquete esquerda
     canvasCtx.fillRect(10, 400, lineWidth, 200)
 
     //Desenho raquete direita
     canvasCtx.fillRect(window.innerWidth - lineWidth - 10, 400,lineWidth, 200)
+
+    //Desenho Bola
+    canvasCtx.beginPath()
+    canvasCtx.arc(500,500,20,0, 2 * Math.PI, false)
+    canvasCtx.fill()
 }
 
 setup()
