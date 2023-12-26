addb552d4b74f1ca430d014dde023da0fd7af781
Carlos Fortunato <carlosfortunatodev@gmail.com>
2023-12-26 19:56:17 -0300

    Desenhando o campo

--- PING-PONG/assets/js/main.js
+++ PING-PONG/assets/js/main.js
@@ -1,3 +1,17 @@
 const canvasEl = document.querySelector("canvas")
 const canvasCtx = canvasEl.getContext("2d")
 const lineWidth = 15;
+
+const setup = () => {
+    canvasEl.width = canvasCtx.width = window.innerWidth
+    canvasEl.height = canvasCtx.height = window.innerHeight
+}
+
+const draw = () =>{
+    //Desenho do Campo
+    canvasCtx.fillStyle = "#286047"
+    canvasCtx.fillRect(0,0, window.innerWidth, window.innerHeight)
+}
+
+setup()
+draw()
